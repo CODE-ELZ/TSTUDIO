000aaa284022a2e597147651d3cf741bbef13205
ELZ <aelzahi@gmail.com>
2021-10-17 23:05:25 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+
+// document.querySelector("#search").style.visibility = "hidden";
+
+// document.querySelector("#search").classList;
+// document.querySelector("#search").classList.add("invisible");
+// document.querySelector("#search").classList.remove("invisible");
+// document.querySelector("#search").classList.toggle("invisible");
+
+
+// muosenter
+// click
+// muoseleave
+
+
+document.getElementById('eve').addEventListener('click', function (){
+    document.querySelector("#nav").classList.toggle("invisibles");
+});
+document.getElementById('evef').addEventListener('click', function (){
+    document.querySelector("#nav").classList.toggle("invisibles");
+});
+document.getElementById('eves').addEventListener('click', function (){
+    document.querySelector("#nav").classList.toggle("invisibles");
+});
+document.getElementById('evex').addEventListener('click', function (){
+    document.querySelector("#nav").classList.toggle("invisibles");
+});
+document.getElementById('evet').addEventListener('click', function (){
+    document.querySelector("#nav").classList.toggle("invisibles");
+});
+
+
+
+
+
